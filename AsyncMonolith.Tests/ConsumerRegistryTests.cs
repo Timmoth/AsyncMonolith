1079ed9c0a51e45a4ef9f9bfbef42097f2196500
timmoth <t.jones@timmoth.com>
2024-06-06 18:44:40 +0100

    Fixed non nullable insert id

--- AsyncMonolith.Tests/ConsumerRegistryTests.cs
+++ AsyncMonolith.Tests/ConsumerRegistryTests.cs
@@ -48,7 +48,7 @@ public class ConsumerRegistryTests
             PayloadType = default!,
             Payload = default!,
             Attempts = default,
-            InsertId = null
+            InsertId = string.Empty
         });
 
         // Then
